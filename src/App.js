b56eacfaea83ae0769f9af3dfdbff8a601b13cf6
mastervr99 <web.dev418@gmail.com>
2024-10-03 19:07:30 +0200

    feat : app components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.scss';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/home';
+import Sign_In from './pages/sign_in/sign_in';
+import User from './pages/user/user';
+import Header from './layout/header/header';
 
 function App() {
   return (
       <Router>
-
+        <Header/>
+        <div className='app_main'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sign-in" element={<Sign_In />} />
+            <Route path="/user/:userId" element={<User />} />
+          </Routes>
+        </div>
       </Router>
     );
 }
