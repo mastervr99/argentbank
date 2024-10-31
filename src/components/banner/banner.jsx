3132732d7fee684ab3cad0e17132b11f00e3cf21
mastervr99 <web.dev418@gmail.com>
2024-10-31 19:59:26 +0100

    refactor : design

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -5,7 +5,7 @@ function Banner() {
     return (
         <div className='banner' style={{ backgroundImage: `url(${banner})`}} >
             <div className='banner-text'>
-                <p className='first-text'>No fees.<br/>No Minimum deposit.<br/>High Interest rates.</p>
+                <p className='first-text'>No fees.<br/>No minimum deposit.<br/>High Interest rates.</p>
                 <p className='second-text'>Open a saving account with<br/>Argent Bank today!</p>
             </div>
         </div>
